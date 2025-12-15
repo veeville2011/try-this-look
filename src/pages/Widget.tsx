1c8991f4869c3334c877552029dedc46cfda0db2
HemanthKumarCreative <avisihks@gmail.com>
2025-12-16 03:52:01 +0530

    Update the root container

--- src/pages/Widget.tsx
+++ src/pages/Widget.tsx
@@ -2,7 +2,7 @@ import TryOnWidget from "@/components/TryOnWidget";
 
 export default function Widget() {
   return (
-    <div className="w-[800px] mx-auto">
+    <div className="w-fit h-auto inline-block">
       <TryOnWidget />
     </div>
   );
